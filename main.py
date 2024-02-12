65cf3647a358a20dc55140dbc19a65f77209c3a7
Sandeep <sandeepsuresh619@gmail.com>
2024-02-13 00:14:00 +0530

    integrated wandb

--- main.py
+++ main.py
@@ -4,24 +4,47 @@ import matplotlib.pyplot as plt
 from keras.utils import to_categorical
 import FFNN
 from tqdm import tqdm
+import wandb
+
 if __name__ == '__main__':
 
-    # Loading the Fashion-MNIST Dataset
-    
     (train_X, train_y), (test_X, test_y) = fashion_mnist.load_data()
-
-    # Ploting the images
-
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-    plt.figure(figsize=(10,10))
-    for i in range(25):
-        plt.subplot(5,5,i+1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.grid(False)
-        plt.imshow(train_X[i], cmap=plt.cm.binary)
-        plt.xlabel(class_names[train_y[i]])
-    plt.show()
+
+    """
+    Question 1.
+    
+    For Ploting the Images , wandb accept in the form of image array and labels. This can be found in the documentation
+
+    The code for plotting to visualize in matplotlib is given below
+
+            class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+            plt.figure(figsize=(10,10))
+            for i in range(25):
+                plt.subplot(5,5,i+1)
+                plt.xticks([])
+                plt.yticks([])
+                plt.grid(False)
+                plt.imshow(train_X[i], cmap=plt.cm.binary)
+                plt.xlabel(class_names[train_y[i]])
+            plt.show()
+
+    """
+    wandb.login()
+
+    run = wandb.init(
+        project ="test"
+    )  
+
+    indices = [list(train_y).index(i) for i in range(10)]
+    images = []
+    labels = []
+    for i in indices:
+        images.append(train_X[i])
+        labels.append(class_names[train_y[i]])
+    plots = [wandb.Image(image, caption=caption) for image, caption in zip(images, labels)] # The plots needs to be an array for logging
+    wandb.log({'examples':plots})
+
 
     """
     Preprocessing the Data to feed into the network
